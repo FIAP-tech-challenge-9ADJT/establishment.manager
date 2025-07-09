3debd2fd908affa8c9701b901dfe6427442534c4
DouglasVulcano <vulcanodouglas1708@gmail.com>
2025-07-09 04:43:21 -0300

    feat: create user and address entites, repositories and dtos

---
+++ src/main/java/tech/challenge/establishment/manager/dtos/address/CreateAddressDTO.java
@@ -0,0 +1,16 @@
+package tech.challenge.establishment.manager.dtos.address;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
+
+public record CreateAddressDTO(
+
+                @NotBlank(message = "Street is required") @Size(max = 100, message = "Street must have at most 100 characters") String street,
+
+                @NotBlank(message = "City is required") @Size(max = 100, message = "City must have at most 100 characters") String city,
+
+                @NotBlank(message = "Postal Code is required") @Size(min = 8, max = 8, message = "Postal Code must have exactly 8 digits") @Pattern(regexp = "^[0-9]{8}$", message = "Postal Code must contain only digits") String postalCode,
+
+                @NotBlank(message = "Number is required") @Size(max = 10, message = "Number must have at most 10 characters") String number) {
+}
---
+++ src/main/java/tech/challenge/establishment/manager/dtos/user/CreateUserDTO.java
@@ -0,0 +1,21 @@
+package tech.challenge.establishment.manager.dtos.user;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import tech.challenge.establishment.manager.dtos.address.CreateAddressDTO;
+
+public record CreateUserDTO(
+
+        @NotBlank(message = "Name is required") @Size(max = 100, message = "Name must have at most 100 characters") String name,
+
+        @NotBlank(message = "Email is required") @Email(message = "Email must be valid") String email,
+
+        @NotBlank(message = "Login is required") @Size(min = 4, max = 50, message = "Login must have between 4 and 50 characters") String login,
+
+        @NotBlank(message = "Password is required") @Size(min = 6, message = "Password must have at least 6 characters") String password,
+
+        @NotNull(message = "Address is required") @Valid CreateAddressDTO address) {
+}
