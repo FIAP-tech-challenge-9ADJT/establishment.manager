e08f1b550f1694ef7bc32e19236a9db3a3ab2a97
Vinícius Ferrarini Chimello <vini_uffs-cc@hotmail.com>
2025-07-09 17:00:30 -0300

    AddingServices
    New UserService and AddressService

---
+++ src/main/java/tech/challenge/establishment/manager/Service/AddressService.java
@@ -0,0 +1,26 @@
+package tech.challenge.establishment.manager.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class AddressService {
+
+    /*
+
+    private AddressRepository addressRepository;
+
+    public AddressService(AddressRepository addressRepository){
+        this.addressRepository = addressRepository;
+    }
+
+    public UUID createAddress(CreateAddressDTO createAddressDTO){
+        var entity = new Address(null, createAddressDTO.street()...);
+        return addressRepository.save(entity).getId();
+    }
+
+    */
+
+}
