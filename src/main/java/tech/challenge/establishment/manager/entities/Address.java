1fd4546fa840ba10da459b548e3685671f6adf1b
DouglasVulcano <vulcanodouglas1708@gmail.com>
2025-07-09 06:50:02 -0300

    feat: implement flyway

--- src/main/java/tech/challenge/establishment/manager/entities/Address.java
+++ src/main/java/tech/challenge/establishment/manager/entities/Address.java
@@ -39,6 +39,11 @@ public class Address implements Serializable {
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+
     @OneToOne(fetch = FetchType.EAGER) 
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false, unique = true)
     @JsonManagedReference
