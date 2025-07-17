5c507ef143c61ed9faa4a7fdb9243bf0436bbbb6
DouglasVulcano <vulcanodouglas1708@gmail.com>
2025-07-16 23:49:34 -0300

    create base file to services
    change 'address' table to 'addresses'
    
    add auxiliary commands section to readme

--- src/main/java/tech/challenge/establishment/manager/controller/AddressController.java
+++ src/main/java/tech/challenge/establishment/manager/controller/AddressController.java
@@ -1,6 +1,5 @@
 package tech.challenge.establishment.manager.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.challenge.establishment.manager.dtos.address.CreateAddressDTO;
